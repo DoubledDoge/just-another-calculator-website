1a4f638e9f466061ae6728b259afd5e13013fd53
Dihan Britz <dbritz22@proton.me>
2025-04-04 00:18:02 +0200

    fix: Attempt to deploy the page on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/just-another-calculator-website/', // Add this line - use your repo name
 })
